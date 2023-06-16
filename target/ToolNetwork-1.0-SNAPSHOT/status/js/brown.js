e68a74ecf9d29c8b00f4645ecec60af370c6eed5
Chuioke <135099538+Chuioke@users.noreply.github.com>
2023-06-16 09:57:23 +0800

    Delete target directory

--- target/ToolNetwork-1.0-SNAPSHOT/status/js/brown.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // Scroll to top when the button is clicked
-    $('#back-to-top-button').on('click', function() {
-        $('.main').scrollTop(0);
-    });
-});
